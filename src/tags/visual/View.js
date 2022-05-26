59c07d0abfc3af37dd8daefd2be4790c8876cb3e
Ian Fox <ianfox41@gmail.com>
2022-05-26 20:30:11 -0300

    [test2] fix

--- src/tags/visual/View.js
+++ src/tags/visual/View.js
@@ -94,7 +94,7 @@ const Model = types
 
 const ViewModel = types.compose("ViewModel", TagAttrs, Model, VisibilityMixin, AnnotationMixin);
 
-const HtxView = observer(({ item, styles }) => {
+const HtxView = observer(({ item, styles = {} }) => {
   let style = {};
 
   if (item.display === "inline") {
